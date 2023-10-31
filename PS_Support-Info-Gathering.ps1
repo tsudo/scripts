6cdcf330abea77bf87897d2178ed4263bf9da147
Keith S. Crawford (tsudo) <tsudo@users.noreply.github.com>
2023-10-30 23:06:13 -0500

    Create PS_Support-Info-Gathering.ps1
    A PowerShell script that creates a support folder structure on the C drive and runs some basic info gathering commands with outputs to the docs folder.

---
+++ PS_Support-Info-Gathering.ps1
@@ -0,0 +1,45 @@
+ ############################################
+# Title: PS_Support_Setup_Script
+# Description: Creates folders for Support and gathers basic info.
+# LastMod: 2023-10-30
+# Author: @tsudo on Github & Twitter
+############################################
+
+############################################
+# DISCLAIMER: Use it an your own risk.
+# Licensed under MIT License https://github.com/tsudo/scripts/blob/master/License.md
+############################################
+
+# Task 1: Create C:\support folder
+New-Item -ItemType Directory -Path "C:\support" -Force
+
+# Task 2: Create C:\support\portable sub-folder
+New-Item -ItemType Directory -Path "C:\support\portable" -Force
+
+# Task 3: Create C:\support\installs sub-folder
+New-Item -ItemType Directory -Path "C:\support\installs" -Force
+
+# Task 4: Create C:\support\docs sub-folder
+New-Item -ItemType Directory -Path "C:\support\docs" -Force
+
+# Task 5: Run "systeminfo" and output to a file
+$systemInfoOutput = systeminfo
+$systemInfoOutput | Out-File -FilePath "C:\support\docs\systeminfo_$(Get-Date -Format 'yyyy-MM-dd').txt"
+
+# Task 6: Run specified commands and put their output in a single text file
+$ipconfigOutput = ipconfig /all
+$serialNumberOutput = wmic bios get serialnumber
+$nslookupOutput = nslookup yahoo.com
+
+$combinedOutput = @"
+IPConfig Output:
+$ipconfigOutput
+
+Serial Number Output:
+$serialNumberOutput
+
+NSLookup Output:
+$nslookupOutput
+"@
+
+$combinedOutput | Out-File -FilePath "C:\support\docs\cmd-docs_$(Get-Date -Format 'yyyy-MM-dd').txt"
